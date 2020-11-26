a448980217d98942be0fc7819372faa7efd89c77
Shane <marusczaks@gmail.com>
2020-11-26 10:50:19 -0500

    rows clearing with all colors and shapes, no rotate

--- js/tetris.js
+++ js/tetris.js
@@ -8,7 +8,7 @@
   const dimensions = {
     rows: 20,
     columns: 10,
-    clockSpeed: 600,
+    clockSpeed: 200,
     pieceSize: 4,
   };
   const gameGrid = [];
@@ -76,6 +76,11 @@
       document
         .getElementById(getCellId(loc[0] + dir, loc[1]))
         .classList.add("moving");
+      copyAndRemoveColorClasses(
+        document.getElementById(getCellId(loc[0], loc[1])),
+        document.getElementById(getCellId(loc[0] + dir, loc[1])),
+        "moving"
+      );
       gameGrid[loc[0] + dir][loc[1]] = 1;
     });
   };
@@ -116,10 +121,12 @@
             .getElementById(getCellId(col, row))
             .classList.remove("placed");
           gameGrid[col][row] = gameGrid[col][row - 1];
+          const color = removeAndReturnColorClasses(col, row);
           if (gameGrid[col][row] === 2) {
             document
               .getElementById(getCellId(col, row))
               .classList.add("placed");
+            document.getElementById(getCellId(col, row)).classList.add(color);
           }
         } else {
           document
@@ -157,6 +164,8 @@
   const drawStraightTetrimino = (col) => {
     for (let i = 0; i < dimensions.pieceSize; i++) {
       document.getElementById(getCellId(col, i)).classList.add("moving");
+      document.getElementById(getCellId(col, i)).classList.add("cyan");
+
       gameGrid[col][i] = 1;
     }
   };
@@ -164,10 +173,14 @@
   const drawSquareTetrimno = (col) => {
     for (let i = 0; i < dimensions.pieceSize / 2; i++) {
       document.getElementById(getCellId(col, i)).classList.add("moving");
+      document.getElementById(getCellId(col, i)).classList.add("yellow");
+
       gameGrid[col][i] = 1;
     }
     for (let i = 0; i < dimensions.pieceSize / 2; i++) {
       document.getElementById(getCellId(col + 1, i)).classList.add("moving");
+      document.getElementById(getCellId(col + 1, i)).classList.add("yellow");
+
       gameGrid[col + 1][i] = 1;
     }
   };
@@ -175,39 +188,56 @@
   const drawLTetrimno = (col) => {
     for (let i = 0; i < dimensions.pieceSize - 1; i++) {
       document.getElementById(getCellId(col, i)).classList.add("moving");
+      document.getElementById(getCellId(col, i)).classList.add("orange");
+
       gameGrid[col][i] = 1;
     }
     document.getElementById(getCellId(col + 1, 2)).classList.add("moving");
+    document.getElementById(getCellId(col + 1, 2)).classList.add("orange");
+
     gameGrid[col + 1][2] = 1;
   };
 
   const drawMirroredLTetrimno = (col) => {
     for (let i = 0; i < dimensions.pieceSize - 1; i++) {
       document.getElementById(getCellId(col, i)).classList.add("moving");
+      document.getElementById(getCellId(col, i)).classList.add("green");
+
       gameGrid[col][i] = 1;
     }
     document.getElementById(getCellId(col - 1, 2)).classList.add("moving");
+    document.getElementById(getCellId(col - 1, 2)).classList.add("green");
+
     gameGrid[col - 1][2] = 1;
   };
 
   const drawTTetrimno = (col) => {
     for (let i = 0; i < dimensions.pieceSize - 1; i++) {
       document.getElementById(getCellId(col, i)).classList.add("moving");
+      document.getElementById(getCellId(col, i)).classList.add("blue");
+
       gameGrid[col][i] = 1;
     }
     document.getElementById(getCellId(col - 1, 1)).classList.add("moving");
+    document.getElementById(getCellId(col - 1, 1)).classList.add("blue");
+
     gameGrid[col - 1][1] = 1;
   };
 
   const drawZTetrimino = (col) => {
     for (let i = 0; i < dimensions.pieceSize / 2; i++) {
       document.getElementById(getCellId(col, i)).classList.add("moving");
+      document.getElementById(getCellId(col, i)).classList.add("purple");
+
       gameGrid[col][i] = 1;
     }
     for (let i = 0; i < dimensions.pieceSize / 2; i++) {
       document
         .getElementById(getCellId(col + 1, i + 1))
         .classList.add("moving");
+      document
+        .getElementById(getCellId(col + 1, i + 1))
+        .classList.add("purple");
       gameGrid[col + 1][i + 1] = 1;
     }
   };
@@ -215,12 +245,15 @@
   const drawMirroredZTetrimino = (col) => {
     for (let i = 0; i < dimensions.pieceSize / 2; i++) {
       document.getElementById(getCellId(col, i)).classList.add("moving");
+      document.getElementById(getCellId(col, i)).classList.add("red");
+
       gameGrid[col][i] = 1;
     }
     for (let i = 0; i < dimensions.pieceSize / 2; i++) {
       document
         .getElementById(getCellId(col - 1, i + 1))
         .classList.add("moving");
+      document.getElementById(getCellId(col - 1, i + 1)).classList.add("red");
       gameGrid[col - 1][i + 1] = 1;
     }
   };
@@ -251,7 +284,6 @@
         drawMirroredZTetrimino(col);
         break;
     }
-
     drawingMode = false;
     fallingMode = true;
   };
@@ -275,6 +307,11 @@
         .getElementById(getCellId(loc[0], loc[1] + 1))
         .classList.add("moving");
       gameGrid[loc[0]][loc[1] + 1] = 1;
+      copyAndRemoveColorClasses(
+        document.getElementById(getCellId(loc[0], loc[1])),
+        document.getElementById(getCellId(loc[0], loc[1] + 1)),
+        "moving"
+      );
     });
   };
 
@@ -306,6 +343,82 @@
     const col = Number(splitId[0]);
     const row = Number(splitId[1]);
     return [col, row];
+  };
+
+  const removeAndReturnColorClasses = (col, row) => {
+    const elem = document.getElementById(getCellId(col, row));
+    const elemAbove = document.getElementById(getCellId(col, row - 1));
+
+    if (elem.classList.contains("blue")) {
+      elem.classList.remove("blue");
+    } else if (elem.classList.contains("red")) {
+      elem.classList.remove("red");
+    } else if (elem.classList.contains("yellow")) {
+      elem.classList.remove("yellow");
+    } else if (elem.classList.contains("green")) {
+      elem.classList.remove("green");
+    } else if (elem.classList.contains("cyan")) {
+      elem.classList.remove("cyan");
+    } else if (elem.classList.contains("purple")) {
+      elem.classList.remove("purple");
+    } else if (elem.classList.contains("orange")) {
+      elem.classList.remove("orange");
+    }
+
+    if (elemAbove.classList.contains("blue")) {
+      return "blue";
+    } else if (elemAbove.classList.contains("red")) {
+      return "red";
+    } else if (elemAbove.classList.contains("yellow")) {
+      return "yellow";
+    } else if (elemAbove.classList.contains("green")) {
+      return "green";
+    } else if (elemAbove.classList.contains("cyan")) {
+      return "cyan";
+    } else if (elemAbove.classList.contains("purple")) {
+      return "purple";
+    } else if (elemAbove.classList.contains("orange")) {
+      return "orange";
+    }
+  };
+
+  const copyAndRemoveColorClasses = (elemFrom, elemTo, classToCheckFor) => {
+    if (elemFrom.classList.contains("blue")) {
+      elemTo.classList.add("blue");
+      if (!elemFrom.classList.contains(classToCheckFor)) {
+        elemFrom.classList.remove("blue");
+      }
+    } else if (elemFrom.classList.contains("red")) {
+      elemTo.classList.add("red");
+      if (!elemFrom.classList.contains(classToCheckFor)) {
+        elemFrom.classList.remove("red");
+      }
+    } else if (elemFrom.classList.contains("yellow")) {
+      elemTo.classList.add("yellow");
+      if (!elemFrom.classList.contains(classToCheckFor)) {
+        elemFrom.classList.remove("yellow");
+      }
+    } else if (elemFrom.classList.contains("green")) {
+      elemTo.classList.add("green");
+      if (!elemFrom.classList.contains(classToCheckFor)) {
+        elemFrom.classList.remove("green");
+      }
+    } else if (elemFrom.classList.contains("cyan")) {
+      elemTo.classList.add("cyan");
+      if (!elemFrom.classList.contains(classToCheckFor)) {
+        elemFrom.classList.remove("cyan");
+      }
+    } else if (elemFrom.classList.contains("purple")) {
+      elemTo.classList.add("purple");
+      if (!elemFrom.classList.contains(classToCheckFor)) {
+        elemFrom.classList.remove("purple");
+      }
+    } else if (elemFrom.classList.contains("orange")) {
+      elemTo.classList.add("orange");
+      if (!elemFrom.classList.contains(classToCheckFor)) {
+        elemFrom.classList.remove("orange");
+      }
+    }
   };
 
   (() => {
